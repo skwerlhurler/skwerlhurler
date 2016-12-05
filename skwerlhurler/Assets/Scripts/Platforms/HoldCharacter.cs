8f1ffd5a972d05fc1018d7bf6291826002b3127e
Nahiyan Khandker <nahiyan.khandker@gmail.com>
2016-12-04 20:04:16 -0600

    adding HoldCharcter & Moving Platform

---
+++ skwerlhurler/Assets/Scripts/Platforms/HoldCharacter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoldCharacter : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		col.transform.parent = gameObject.transform;
+	}
+
+	void OnTriggerExit(Collider col){
+		col.transform.parent = null;
+	}
+}
